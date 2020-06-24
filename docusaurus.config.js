57b777b361a18e14fd032fea80f472f7b342d59d
csgeek <samir@esamir.com>
2020-06-24 11:10:45 -0700

    Added official Logo, updated the reference links (#15)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,85 +1,118 @@
 module.exports = {
-  title: 'Geek Beacon Documentation Site',
-  tagline: 'Geek Beacon Is Your Guiding Light To All Things Geek!',
-  url: 'https://geekbeacon.github.io',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'GeekBeacon', // Usually your GitHub org/user name.
-  projectName: 'geekbeacon.github.io', // Usually your repo name.
+  title: "Welcome to GeekBeacon",
+  tagline: "Geek Beacon Is Your Guiding Light To All Things Geek!",
+  url: "https://geekbeacon.github.io",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "GeekBeacon", // Usually your GitHub org/user name.
+  projectName: "geekbeacon.github.io", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Geek Beacon Documentation',
+      title: "Geek Beacon Documentation",
       logo: {
-        alt: 'GB Logo',
-        src: 'img/logo.png',
+        alt: "GB Logo",
+        src: "img/logo.png",
       },
       links: [
         {
-          to: 'docs/intro/welcome',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/intro/welcome",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
         {
-          to: 'blog', 
-          label: 'Blog', 
-          position: 'left',
+          to: "blog",
+          label: "Blog",
+          position: "left",
           postsPerPage: 3,
-          blogSidebarCount: 'ALL',
+          blogSidebarCount: "ALL",
           feedOptions: {
-            type: 'all'
-          }
-
+            type: "all",
+          },
         },
         {
-          href: '/blog/tags/engineering',
-          label: 'Tech Blog',
-          position: 'left',
-          target: '_self'
+          href: "/blog/tags/engineering",
+          label: "Tech Blog",
+          position: "left",
+          target: "_self",
         },
         {
-          href: 'https://github.com/GeekBeacon/geekbeacon-docs',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/GeekBeacon/geekbeacon-docs",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     algolia: {
-      apiKey: '266aced93096c0cb9effe165264556f4',
-      indexName: 'geekbeacon',
+      apiKey: "266aced93096c0cb9effe165264556f4",
+      indexName: "geekbeacon",
       //appId: '', // Optional, if you run the DocSearch crawler on your own
       //algoliaOptions: {}, // Optional, if provided by Algolia
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Main Website",
+              href: "https://www.geekbeacon.org",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Forum",
+              href: "https://forum.geekbeacon.org",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Mailing Lists",
+              href: "https://lists.geekbeacon.org",
             },
           ],
         },
         {
-          title: 'Social',
+          title: "Tech",
           items: [
             {
-              label: 'MeetingNotes',
-              to: 'blog',
+              label: "GitHub",
+              href: "https://github.com/Geek-Beacon/geekbeacon-docs",
+            },
+          ],
+        },
+        {
+          title: "GB Social",
+          items: [
+            {
+              label: "YouTube",
+              href: "https://www.youtube.com/channel/UCG_4Qltzr18ngVyoyOg-frQ",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/Geek-Beacon/geekbeacon',
+              label: "Facebook",
+              href: "https://www.facebook.com/GeekBeacon/",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/geekbeacon',
+              label: "Twitter",
+              href: "https://twitter.com/geekbeacon",
+            },
+          ],
+        },
+        {
+          title: "Nixie Social",
+          items: [
+            {
+              label: "YouTube",
+              href: "https://twitter.com/nixiepixel",
+            },
+            {
+              label: "Facebook",
+              href: "https://www.facebook.com/nixiepixel/",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/nixiepixel",
             },
           ],
         },
@@ -89,16 +122,15 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/GeekBeacon/geekbeacon-docs/edit/master/'
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/GeekBeacon/geekbeacon-docs/edit/master/",
           // routeBasePath: '/', // Set this value to '/'.
-
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,62 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
-    title: <><a href="https://www.youtube.com/channel/UC8OOBdn0YNSUszZV98a3CLA">YouTube</a></>,
-    imageUrl: 'img/gb/youtube_cube.png',
+    title: (
+      <>
+        <a href="https://www.youtube.com/channel/UC8OOBdn0YNSUszZV98a3CLA">
+          YouTube
+        </a>
+      </>
+    ),
+    imageUrl: "img/gb/youtube_cube.png",
     description: (
       <>
-       Subscribe to us on Youtube and follow us as we search for the ultimate in geeky passion that lights the great beacon!
+        Subscribe to us on Youtube and follow us as we search for the ultimate
+        in geeky passion that lights the great beacon!
       </>
     ),
   },
   {
-    title: <><a href="https://www.twitch.tv/nixiepixel">Twitch</a></>,
-    imageUrl: 'img/gb/gaming_cube.png',
+    title: (
+      <>
+        <a href="https://www.twitch.tv/nixiepixel">Twitch</a>
+      </>
+    ),
+    imageUrl: "img/gb/gaming_cube.png",
     description: (
       <>
-      Join us on Twitch and see our shenanigans live! Come and geek out with us live and join our new geeky Twitch gaming community!
+        Join us on Twitch and see our shenanigans live! Come and geek out with
+        us live and join our new geeky Twitch gaming community!
       </>
     ),
   },
   {
-    title: <><a href="https://discord.gg/geekbeacon">Discord</a></>,
-    imageUrl: 'img/gb/discord_cube.png',
+    title: (
+      <>
+        <a href="https://discord.gg/geekbeacon">Discord</a>
+      </>
+    ),
+    imageUrl: "img/gb/discord_cube.png",
     description: (
       <>
-      Come join us live on Discord! Be involved with our community and join in with our Geeky Shenanigans live on Discord! Come join our squirrels.
+        Come join us live on Discord! Be involved with our community and join in
+        with our Geeky Shenanigans live on Discord! Come join our squirrels.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={classnames('col col--4', styles.feature)}>
+    <div className={classnames("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -53,16 +70,20 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      title={`${siteConfig.title}`}
+      description="GeekBeacon Community Documentation and News"
+    >
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
+          <img src="/img/official.png" width="40%" height="40%" />
           <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <p className="hero__subtitle"><a href="/docs/welcome">Getting Started</a></p>
+          <p className="hero__subtitle">
+            <a href="/docs/welcome">Getting Started</a>
+          </p>
         </div>
       </header>
       <main>
